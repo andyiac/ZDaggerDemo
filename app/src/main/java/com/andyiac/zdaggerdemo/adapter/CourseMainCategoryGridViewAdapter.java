c44880ef4e09127f021ed0098d92b1642e96f1af
andyiac <andyiaczhang@gmail.com>
2015-12-31 13:11:25 +0800

    fix view

--- app/src/main/java/com/andyiac/zdaggerdemo/adapter/CourseMainCategoryGridViewAdapter.java
+++ app/src/main/java/com/andyiac/zdaggerdemo/adapter/CourseMainCategoryGridViewAdapter.java
@@ -1,8 +1,6 @@
 package com.andyiac.zdaggerdemo.adapter;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.Build;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
